090d830f935cc356dc38eded9ff5d1f74b8696c0
Himalee <himalee.tailor@gmail.com>
2022-05-02 12:15:14 +0100

    Return word for numbers 21-100

--- lib/number_converter.rb
+++ lib/number_converter.rb
@@ -15,13 +15,27 @@ class NumberConverter
 			12 => "twelve",
 			15 => "fifteen",
 			20 => "twenty",
+      30 => "thirty",
+      40 => "forty",
+      50 => "fifty",
+      60 => "sixty",
+      70 => "seventy",
+      80 => "eighty",
+      90 => "ninety",
+			100 => "one hundred",
 		}
+
+		digits = number.digits.reverse
 
 		if number == 15
 			return "fifteen"
 		elsif number.between?(13, 19)
-			first_digit = number.digits[0]
-			return "#{number_in_words[first_digit]}teen"
+			second_digit = digits[1]
+			return "#{number_in_words[second_digit]}teen"
+		elsif number > 20 && digits[1] != 0
+			first_digit = digits[0]
+			second_digit = digits[1]
+			"#{number_in_words[first_digit*10]}-#{number_in_words[second_digit]}"
 		else
 			return number_in_words[number]
 		end
--- spec/number_converter_spec.rb
+++ spec/number_converter_spec.rb
@@ -15,4 +15,12 @@ describe NumberConverter do
 		expect(@number_converter.convert(17)).to eq("seventeen")
 		expect(@number_converter.convert(20)).to eq("twenty")
 	end
+
+	it "returns word for numbers 21-100" do
+		expect(@number_converter.convert(21)).to eq("twenty-one")
+		expect(@number_converter.convert(50)).to eq("fifty")
+		expect(@number_converter.convert(63)).to eq("sixty-three")
+		expect(@number_converter.convert(99)).to eq("ninety-nine")
+		expect(@number_converter.convert(100)).to eq("one hundred")
+	end
 end
