190b2559edb5ea73955fb745d9ac9ce3d17e2e18
Himalee <himalee.tailor@gmail.com>
2022-05-02 11:57:41 +0100

    Return word for numbers 11-20

--- lib/number_converter.rb
+++ lib/number_converter.rb
@@ -11,8 +11,19 @@ class NumberConverter
       8 => "eight",
       9 => "nine",
 			10 => "ten",
+			11 => "eleven",
+			12 => "twelve",
+			15 => "fifteen",
+			20 => "twenty",
 		}
 
-		number_in_words[number]
+		if number == 15
+			return "fifteen"
+		elsif number.between?(13, 19)
+			first_digit = number.digits[0]
+			return "#{number_in_words[first_digit]}teen"
+		else
+			return number_in_words[number]
+		end
 	end
 end
--- spec/number_converter_spec.rb
+++ spec/number_converter_spec.rb
@@ -8,4 +8,11 @@ describe NumberConverter do
 		expect(@number_converter.convert(1)).to eq("one")
 		expect(@number_converter.convert(10)).to eq("ten")
 	end
+
+	it "returns word for numbers 11-20" do
+		expect(@number_converter.convert(12)).to eq("twelve")
+		expect(@number_converter.convert(15)).to eq("fifteen")
+		expect(@number_converter.convert(17)).to eq("seventeen")
+		expect(@number_converter.convert(20)).to eq("twenty")
+	end
 end
